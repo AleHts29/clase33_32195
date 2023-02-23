5b5a2f1e95256635516df1299042fa254a0acd92
Alejandro Huertas <ahuertas@fi.uba.ar>
2023-02-23 11:04:20 -0300

    estructura base del proyecto

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-console.log('Hola Mundo');
+import express from 'express';
+const app = express();
+
+let PORT = process.env.PORT || 8080
 
 
-console.log('otro Hola Mundo');
+app.get('/', (req, res) => {
+    res.send('Hola Git')
+})
+
+app.get('/ruta-dev', (req, res) => {
+    res.send('Hola dev')
+})
+
+app.listen(PORT, () => {
+    console.log(`Server run on port: ${PORT}`);
+})
